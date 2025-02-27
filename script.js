c4379e0ca73d69f5c574394aa8a0c530c052ce6b
Alex Harrison <alexharrison96@yahoo.com>
2025-02-27 11:50:35 -0600

    update
    updated the whole site graphically with better nav, added home nav button, created consistent backround image, removed lightmode option due to new backround image, cleaned up some code

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
 new WOW().init();
 
 document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.getElementById("themeToggle");
-
-    // Check local storage for theme preference and apply it
-    if (localStorage.getItem("theme") === "light") {
-        document.body.classList.add("light-theme");
-        toggleButton.textContent = "Dark Theme"; // Set button text to Dark Theme
-    } else {
-        document.body.classList.add("dark-theme");
-        toggleButton.textContent = "Light Theme"; // Set button text to Light Theme
-    }
-
-    toggleButton.addEventListener("click", function () {
-        // Toggle between light and dark theme
-        document.body.classList.toggle("light-theme");
-        document.body.classList.toggle("dark-theme");
-
-        // Save the preference in local storage and update button text
-        if (document.body.classList.contains("light-theme")) {
-            localStorage.setItem("theme", "light");
-            toggleButton.textContent = "Dark Theme"; // Change button text to Dark Theme
-        } else {
-            localStorage.setItem("theme", "dark");
-            toggleButton.textContent = "Light Theme"; // Change button text to Light Theme
-        }
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
     // Initialize EmailJS with user ID
     emailjs.init('lzL0DNlABawdysupm');  
 
